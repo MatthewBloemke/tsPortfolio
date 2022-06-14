42bdeef61ada7b7df32d49c4a88696f93b04cabc
MatthewBloemke <mattbloemke@gmail.com>
2022-06-14 01:23:26 +0000

    added router

--- src/App.tsx
+++ src/App.tsx
@@ -1,9 +1,12 @@
 import Layout from './Layout/Layout'
+import {BrowserRouter as Router} from 'react-router-dom'
 
 function App() {
   return (
     <div className="App">
-      <Layout/>
+      <Router>
+        <Layout/>
+      </Router>
     </div>
   );
 }
